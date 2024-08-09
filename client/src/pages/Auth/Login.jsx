d6a5560c27c690d6786d31f295e5f258a0a72cc8
Magin Madhavan <magin@Magins-MacBook-Air.local>
2024-08-09 14:50:02 +0530

    Add feature to change password by answering security question incase user forgets password

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -54,6 +54,9 @@ const Login = () => {
 <div className="mb-3">
   <input required placeholder="Password" type="password" className="form-control" id="exampleInputPassword" value={password} onChange={(e)=>setPassword(e.target.value)}/>
 </div>
+<div className="mb-3">
+<button type="button" className="btn btn-primary" onClick={()=>{navigate('/forgot-password')}}>Forgot Password</button>
+</div>
 
 <button type="submit" className="btn btn-primary">Login</button>
 </form>
