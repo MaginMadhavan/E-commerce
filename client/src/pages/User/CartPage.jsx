27474ee76232c2f91c54ee8559de01c86d3f9c72
Magin Madhavan <magin@Magins-MacBook-Air.local>
2024-08-20 18:33:50 +0530

    Add Cart Page

---
+++ client/src/pages/User/CartPage.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import Layout from "../../components/Layouts/Layout";
+import { useCart } from "../../context/cart";
+import { useAuth } from "../../context/auth";
+import { useNavigate } from "react-router-dom";
+
+const CartPage = () => {
+  const [cart, setCart] = useCart();
+  const [auth, setAuth] = useAuth();
+  const navigate = useNavigate();
+  const totalPrice = () => {
+    try {
+      let total = 0;
+      cart?.map((item) => {
+        total = total + item.price;
+      });
+      return total.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeCartItem = (pid) => {
+    try {
+      let myCart = [...cart];
+      let index = myCart.findIndex((item) => item._id === pid);
+      myCart.splice(index, 1);
+      setCart(myCart);
+      localStorage.setItem("cart", JSON.stringify(myCart));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Layout>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="text-center bg-light p-2">
+              {`Hello ${auth?.token && auth?.user.name}`}
+            </h1>
+            <h4 className="text-center">
+              {cart?.length
+                ? ` You have ${cart?.length} items in your cart ${
+                    auth?.token ? "" : ", please login to checkout"
+                  }`
+                : " Your Cart is empty "}
+            </h4>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-8">
+            <div className="row">
+              {cart?.map((p) => (
+                <div className="row mb-2 card flex-row p-3">
+                  <div className="col-md-4">
+                    <img
+                      src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                      className="card-img-top"
+                      alt={p.name}
+                      height="100px"
+                    />
+                  </div>
+                  <div className="col-md-7">
+                    <p>{p.name}</p>
+                    <p>{p.description.substring(0, 30)}</p>
+                    <p>Rs.{p.price}</p>
+                  </div>
+                  <div className="col-md-1">
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => removeCartItem(p._id)}
+                    >
+                      X
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="col-md-4 text-center">
+            <h2> Cart Summary </h2>
+            <p>Total | Checkout | Payment</p>
+            <hr />
+            <h4>Total : {totalPrice()}</h4>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default CartPage;
