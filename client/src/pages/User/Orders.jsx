648f075a89a41b5d79ed4ed64a381d9914ff4820
Magin Madhavan <magin@Magins-MacBook-Air.local>
2024-08-21 00:23:19 +0530

    Add orderList

--- client/src/pages/User/Orders.jsx
+++ client/src/pages/User/Orders.jsx
@@ -1,25 +1,87 @@
-import React from 'react'
-import Layout from '../../components/Layouts/Layout'
-import UserMenu from '../../components/Layouts/UserMenu'
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/Layouts/Layout";
+import UserMenu from "../../components/Layouts/UserMenu";
+import axios from "axios";
+import { useAuth } from "../../context/auth";
+import moment from "moment";
 
 const Orders = () => {
+  const [orders, setOrders] = useState([]);
+  const [auth, setAuth] = useAuth();
+  const getOrders = async () => {
+    try {
+      const { data } = await axios.get(
+        `http://localhost:8080/api/v1/auth/orders`
+      );
+      setOrders(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    if (auth?.token) getOrders();
+  }, [auth?.token]);
   return (
     <Layout title={"Your Orders"}>
-        <div className="container-fluid p-3 m-3">
+      <div className="container-fluid p-3 m-3">
         <div className="row">
-        <div className="col-md-3">
-            <UserMenu/>
+          <div className="col-md-3">
+            <UserMenu />
+          </div>
+          <div className="col-md-9">
+            <h1 className="text-center">All Orders</h1>
+            {orders?.map((o, i) => {
+              return (
+                <div className="border shadow">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <td scope="col">#</td>
+                        <td scope="col">Status</td>
+                        <td scope="col">Buyer</td>
+                        <td scope="col">date</td>
+                        <td scope="col">Payment</td>
+                        <td scope="col">Quantity</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th>{i + 1}</th>
+                        <th>{o?.status}</th>
+                        <th>{o?.buyer?.name}</th>
+                        <th>{moment(o?.createdAt).fromNow()}</th>
+                        <th>{o?.payment.success ? "Success" : "Failed"}</th>
+                        <th>{o?.products?.length}</th>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="container">
+                    {o?.products?.map((p, i) => (
+                      <div className="row mb-2 card flex-row p-3">
+                        <div className="col-md-4">
+                          <img
+                            src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                            className="card-img-top"
+                            alt={p.name}
+                            height="100px"
+                          />
+                        </div>
+                        <div className="col-md-7">
+                          <p>{p.name}</p>
+                          <p>{p.description.substring(0, 30)}</p>
+                          <p>Rs.{p.price}</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
-        <div className="col-md-9">
-            <h1>All Orders</h1>
-        </div>
-        </div>  
-       
-
-        </div>
-        
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
